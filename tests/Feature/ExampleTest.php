815b85760c99a4d8d4d0ffc2b4b5418fa1bdd298
Damien van den IJssel <dovandenijssel@live.nl>
2025-05-09 13:44:19 +0200

    Commit after merge

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,7 +12,6 @@ class ExampleTest extends TestCase
     public function test_returns_a_successful_response()
     {
         $response = $this->get('/');
-
         $response->assertStatus(200);
     }
 }
