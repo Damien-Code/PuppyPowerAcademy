f6bc6addf5de6c30481bac9d7ff8c53755a7f092
Floris Hafkenscheid <floris@hafkenscheid.com>
2025-05-12 13:26:58 +0200

    feat: Contact view

--- resources/js/pages/settings/admin/Contact.vue
+++ resources/js/pages/settings/admin/Contact.vue
@@ -1,28 +1,53 @@
 <script setup lang="ts">
 import { Head } from '@inertiajs/vue3';
 
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Label } from '@/components/ui/label'
+import { Switch } from '@/components/ui/switch'
+
 import AppearanceTabs from '@/components/AppearanceTabs.vue';
 import HeadingSmall from '@/components/HeadingSmall.vue';
 import { type BreadcrumbItem } from '@/types';
+import { reactive } from 'vue';
 
 import AppLayout from '@/layouts/AppLayout.vue';
 import SettingsLayout from '@/layouts/settings/Layout.vue';
 
 const breadcrumbItems: BreadcrumbItem[] = [
     {
-        title: 'Appearance settings',
-        href: '/settings/appearance',
+        title: 'Admin panel',
+        href: '/settings/admin',
     },
 ];
 </script>
 
 <template>
     <AppLayout :breadcrumbs="breadcrumbItems">
-        <Head title="Admin Contact" />
+        <Head title="Admin Panel"/>
 
         <SettingsLayout>
             <div class="space-y-6">
-                <HeadingSmall title="Contact" description="Update your account's appearance settings" />
+                <HeadingSmall title="Contact" description="View your unread contact messages"/>
+                <Table class="">
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Naam</TableHead>
+                            <TableHead>E-Mail</TableHead>
+                            <TableHead class="max-w-1/2">Bericht</TableHead>
+                            <TableHead class="text-right w-1/12">Afgerond</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow v-for="message in messages" :key="message.id" class="">
+                            <TableCell>{{ message.name }}</TableCell>
+                            <TableCell>{{ message.email }}</TableCell>
+                            <TableCell class="max-w-1/2 overflow-scroll">{{ message.message }}</TableCell>
+                            <TableCell class="flex">
+                                <Switch class="ml-auto" id="is-read" v-model="message.is_completed" />
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
             </div>
         </SettingsLayout>
     </AppLayout>
