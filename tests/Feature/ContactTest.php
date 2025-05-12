b9a4de01e54ffaed1a6c45ce3d6597d01edcabfe
Damien van den IJssel <dovandenijssel@live.nl>
2025-05-12 18:30:51 +0200

    Created two contact tests

---
+++ tests/Feature/ContactTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ContactTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic test where the contact page can be rendered without login.
+     */
+    public function test_contactPageCanBeRenderedWithoutLogin(): void
+    {
+        $response = $this->get('/contact');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_user_can_send_contact_message(): void
+    {
+        // Data to be sent to the store method
+        $data = [
+            'first_name' => 'Test',
+            'last_name' => 'User',
+            'email' => 'test@example.com',
+            'message' => 'Test Message',
+            'is_completed' => false,
+        ];
+        // Send a POST request to the store route
+        $response = $this->post(route('contact.store'), $data);
+        // Assert that the response is a redirect
+        $response->assertStatus(302);
+        $this->assertDatabasehas('contacts', $data);
+
+    }
+}
