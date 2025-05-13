5a04b120281fceba14857bb40c14c3ad09d344df
Mischa Sasse <mmischa1@gmail.com>
2025-05-13 22:37:01 +0200

    re-added back migration id in order to make cart_products update work properly

--- database/migrations/2025_05_07_133651_create_cart__products_table.php
+++ database/migrations/2025_05_07_133651_create_cart__products_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->foreignId('product_id')->constrained('products');
             $table->integer('amount');
             $table->timestamps();
+  
         });
     }
 
