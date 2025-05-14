d5bdef830742957d02d651fddfafa8a32c0dabe4
Damien van den IJssel <dovandenijssel@live.nl>
2025-05-14 10:12:10 +0200

    Commit before checkout

--- app/Http/Controllers/Admin/AdminWebshopController.php
+++ app/Http/Controllers/Admin/AdminWebshopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -13,7 +14,9 @@ class AdminWebshopController extends Controller
      */
     public function index()
     {
-        return Inertia::render('settings/admin/Webshop');
+        return Inertia::render('settings/admin/Webshop', [
+            'products' => Product::all()
+        ]);
     }
 
     /**
