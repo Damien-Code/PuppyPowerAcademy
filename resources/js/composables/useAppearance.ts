3df6744502c5f8db130fde28c41837d25785a00c
Damien van den IJssel <dovandenijssel@live.nl>
2025-05-23 10:23:29 +0200

    Forced lightmode

--- resources/js/composables/useAppearance.ts
+++ resources/js/composables/useAppearance.ts
@@ -1,6 +1,6 @@
 import { onMounted, ref } from 'vue';
 
-type Appearance = 'light' | 'dark' | 'system';
+type Appearance = 'light' | 'system';
 
 export function updateTheme(value: Appearance) {
     if (typeof window === 'undefined') {
@@ -8,12 +8,12 @@ export function updateTheme(value: Appearance) {
     }
 
     if (value === 'system') {
-        const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
-        const systemTheme = mediaQueryList.matches ? 'dark' : 'light';
+        const mediaQueryList = window.matchMedia('(prefers-color-scheme: light)');
+        const systemTheme = mediaQueryList.matches ? 'light' : 'light';
 
-        document.documentElement.classList.toggle('dark', systemTheme === 'dark');
+        document.documentElement.classList.toggle('light', systemTheme === 'light');
     } else {
-        document.documentElement.classList.toggle('dark', value === 'dark');
+        document.documentElement.classList.toggle('light', value === 'light');
     }
 }
 
