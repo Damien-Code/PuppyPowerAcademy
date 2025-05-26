7fbc0ebd4ae3aa1f55e102ce9cffb86960a5e583
Floris Hafkenscheid <floris@hafkenscheid.com>
2025-05-26 17:29:31 +0200

    fix: Merge conflicts

--- resources/js/pages/Contact.vue
+++ resources/js/pages/Contact.vue
@@ -85,7 +85,7 @@ const submit = () => {
     <AppLayout :breadcrumbs="breadcrumbs">
         <Toaster/>
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-            <div class="border-sidebar-border/70 dark:border-sidebar-border relative mx-auto rounded-xl border md:min-h-min bg-white p-8">
+            <div class="border-sidebar-border/7 relative mx-auto rounded-xl border md:min-h-min bg-white p-8">
                 <Heading title="Contact" description="Mocht u vragen hebben, stel ze gerust via dit contactformulier"/>
                 <form class="flex flex-col justify-between gap-4 p-4 md:w-96 min-h-full" @submit.prevent="submit">
                     <Label for="first_name">Voornaam</Label>
