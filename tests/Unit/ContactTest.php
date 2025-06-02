e63a596780c145b79ef5c46a8e909ae58e145e48
Damien van den IJssel <dovandenijssel@live.nl>
2025-06-02 10:23:10 +0200

    Started with acceptancetest for contact

--- tests/Unit/ContactTest.php
+++ tests/Unit/ContactTest.php
@@ -1,24 +1,13 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Unit;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ContactTest extends TestCase
 {
     use RefreshDatabase;
-    /**
-     * @author Damien-Code
-     * A basic test where the contact page can be rendered without login.
-     */
-    public function test_contactPageCanBeRenderedWithoutLogin(): void
-    {
-        $response = $this->get('/contact');
-
-        $response->assertStatus(200);
-    }
 
     /**
      * @author  Damien-Code
@@ -39,6 +28,20 @@ class ContactTest extends TestCase
         // Assert that the response is a redirect
         $response->assertStatus(302);
         $this->assertDatabasehas('contacts', $data);
+    }
 
+    public function test_user_can_send_contact_message_and_admin_can_complete_message(): void
+    {
+        // Data to be sent to the store method
+        $data = [
+            'first_name' => 'Test',
+            'last_name' => 'User',
+            'email' => 'test@example.com',
+            'message' => 'Test Message',
+            'completed_at' => null,
+        ];
+        // Send a POST request to the store route
+        $response = $this->post('/contact', $data);
+        $
     }
 }
