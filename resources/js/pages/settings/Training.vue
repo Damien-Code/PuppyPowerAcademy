d1ef7d2a0dc31462f652bccec4077e9e20282432
Floris Hafkenscheid <floris@hafkenscheid.com>
2025-06-11 14:11:48 +0200

    feat: Indicator when no trainings have been purchased

--- resources/js/pages/settings/Training.vue
+++ resources/js/pages/settings/Training.vue
@@ -47,8 +47,7 @@ defineProps<Props>();
             <div class="space-y-6">
                 <HeadingSmall title="Training" description="Bekijk hier uw bestelde trainingen" />
                 
-                <!-- Training categories (always show) -->
-                <div>
+                <div v-if="trainingCategories && trainingCategories.length > 0">
                     <div v-for="category in trainingCategories" :key="category.id" class="bg-primary rounded-lg p-8">
                         <Heading :title="category.name"/>
                         <div v-if="category.trainings && category.trainings.length > 0">
@@ -92,6 +91,19 @@ defineProps<Props>();
                         </div>
                         <div v-else>
                             <p>Video's worden zo snel mogelijk toegevoegd aan dit trainings pakket.</p>
+                        </div>
+                    </div>
+                </div>
+                <div v-else class="bg-yellow-50 border border-yellow-200 rounded-lg p-6">
+                    <div class="flex items-center">
+                        <svg class="w-6 h-6 text-yellow-600 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        </svg>
+                        <div>
+                            <h3 class="text-lg font-medium text-yellow-800">Geen training aangeschaft</h3>
+                            <p class="text-yellow-700 mt-1">
+                                Om trainingen te bekijken moet u deze eerst aanschaffen.
+                            </p>
                         </div>
                     </div>
                 </div>
